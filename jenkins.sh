4d537b16d20cd1724aaf1335b2ed3aec6cad3cfb
wadalkondeashitosh <wadalkondea@gmail.com>
2025-02-08 13:26:12 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,10 +2,10 @@
 yum install git java-1.8.0-openjdk maven -y
 
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-#STEP-3: DOWNLOAD JAVA11 AND JENKINS
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+#STEP-3: DOWNLOAD JAVA17 AND JENKINS
 sudo yum install java-17-amazon-corretto -y
 yum install jenkins -y
 update-alternatives --config java
