6a000a1d2604f3dbe7e3bd6ad8d80e8480dc8b3d
wadalkondeashitosh <wadalkondea@gmail.com>
2025-04-04 19:12:26 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,5 +1,8 @@
 #create amazonlinux ec2 with t2.micro and 30 gb of ebs with port 8081 
-
+sudo yum update -y
+sudo yum install wget -y
+sudo yum install java-1.8.0-openjdk.x86_64 -y
+sudo mkdir /app && cd /app
 wget https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.79.0-09.tar.gz\
 tar -zxvf nexus-unix-x86-64-3.79.0-09.tar.gz
 yum install java-17-amazon-corretto -y
