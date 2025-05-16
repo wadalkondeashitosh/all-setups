84a84116d35f8429aabe95281027d75df262d2d4
wadalkondeashitosh <wadalkondea@gmail.com>
2025-05-16 15:30:34 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -3,7 +3,7 @@ wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.41/bin/apache-tomcat-10.1.4
 tar -zxvf apache-tomcat-10.1.41.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.41/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.41/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.41/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="Ashu@2612" roles="manager-gui, manager-script"/>' apache-tomcat-10.1.41/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-10.1.41/conf/tomcat-users.xml
 sed -i '56d' apache-tomcat-10.1.41/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-10.1.41/webapps/manager/META-INF/context.xml
